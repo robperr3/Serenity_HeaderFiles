258fed45e2abc60116e446632dd247734fcf2e47
robperr3 <39318454+robperr3@users.noreply.github.com>
2018-06-22 13:57:50 -0700

    Add files via upload

---
+++ Dialog.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "Dialog.h"
+
+using namespace std;
+
+void Mal(string texttoSpeak)
+{
+	cout << texttoSpeak << endl;
+}
+void Jayne(string texttoSpeak)
+{
+	cout << texttoSpeak << endl;
+}
