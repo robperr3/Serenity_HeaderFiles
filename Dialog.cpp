522c5154aba52a73280d9f0b8148a2917df8e8bd
robperr3 <39318454+robperr3@users.noreply.github.com>
2018-06-26 18:44:42 -0400

    Add files via upload

--- Dialog.cpp
+++ Dialog.cpp
@@ -1,15 +1,15 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
-#include "Dialog.h"
-
+#include "Dialog.h"//inlcuding self-made header file
+//This source file defines all the functions so you dont have to in the main file
 using namespace std;
 
-void Mal(string texttoSpeak)
+void Mal(string texttoSpeak)//Mal function prints out whatever string you input as an argument
 {
-	cout << texttoSpeak << endl;
+	cout << texttoSpeak << endl;//printing the string argument
 }
-void Jayne(string texttoSpeak)
+void Jayne(string texttoSpeak)//Jayne function prints out whatevever string you input as an argument
 {
-	cout << texttoSpeak << endl;
+	cout << texttoSpeak << endl;//printing the string argument
 }
--- Dialog.h
+++ Dialog.h
@@ -2,8 +2,9 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
-
+//by declaring the functions in this header file the main file doesnt have to dclsre them 
+//as long as the main file includes this header file first
 using namespace std;
 
-void Mal(string);
-void Jayne(string);
+void Mal(string);//declaring the "Mal" function and having a string as the argument
+void Jayne(string);//declaring the "Jayne" function and having a striong as the argument
