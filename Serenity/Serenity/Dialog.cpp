a330c82c3ab7b99617e5c8e3a93a826ed413fb56
robperr3 <39318454+robperr3@users.noreply.github.com>
2018-06-26 23:15:33 -0400

    Add files via upload

---
+++ Serenity/Serenity/Dialog.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>//including the string preprocessor since i want to use strings
+#include "Dialog.h"//inlcuding self-made header file
+//This source file defines all the functions so you dont have to in the main file
+using namespace std;
+
+void Mal(string texttoSpeak)//Mal function prints out whatever string you input as an argument
+{
+	cout << texttoSpeak << endl;//printing the string argument
+}
+void Jayne(string texttoSpeak)//Jayne function prints out whatevever string you input as an argument
+{
+	cout << texttoSpeak << endl;//printing the string argument
+}
---
+++ Serenity/Serenity/Dialog.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "stdafx.h"
+#include <iostream>
+#include <string>//including the string preprocessor since i want to use strings
+//by declaring the functions in this header file the main file doesnt have to declare them
+//as long as the main file includes this header file first
+using namespace std;
+
+void Mal(string);//declaring the "Mal" function and having a string as the argument
+void Jayne(string);//declaring the "Jayne" function and having a striong as the argument
